734fb6b166b902333b3a80b133ea4a55babc4a62
ezekiel-1 <ezekielmuito671@gmail.com>
2023-02-18 00:25:39 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "programming is like building a multilingual"
+ * main - prints "programming is like building a multilingual
  * puzzle, followed by a newline."
  * return 0.
  */
@@ -9,7 +9,7 @@
 int main(void)
 
 {
-	puts("\"programming is like building a multilingual puzzle);
+	puts("\"programming is like building a multilingual puzzle");
 
 	return(0);
 }
