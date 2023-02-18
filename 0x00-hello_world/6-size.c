cd3a2f507f42a27a4f8955960a11651f7af22746
ezekiel-1 <ezekielmuito671@gmail.com>
2023-02-18 03:53:03 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,7 @@
  * Return: Always 0.
  */
 
+
 int main(void)
 
 {
@@ -18,4 +19,4 @@ int main(void)
 	printf("Size of a float %zu byte(s)\n", sizeof(float));
 	return (0);
 }
- 
+"6-size.c" [New] 20l, 487 written
