583fb3e270aa7e933cb2d0dd6212a5c3f30d5ca4
ezekiel-1 <ezekielmuito671@gmail.com>
2023-02-19 22:59:01 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,22 +16,26 @@ int main(void)
 
 	while (d < 10)
 	{
-		if (d != c && d < c)
+		c = 0;
+		while (c < 10)
 		{
-			putchar('0' + d);
-			putchar('0' + c);
-			
-			if (c + d != 17)
+			if (d != c && d < c)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar('0' + d);
+				putchar('0' + c);
+				
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
+			
+			c++;
 		}
-		c++;
+		d++;
 	}
-	d++;
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
 
