5ec2680982506afa0c8d9084ceff47139872a600
ezekiel-1 <ezekielmuito671@gmail.com>
2023-02-27 12:06:18 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,10 +17,13 @@ void print_times_table(int n)
 				k = j * i;
 				if (j == 0)
 				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
+					_putcahr(' ');
 					_putchar(k + '0');
 				} else if (k >= 10 && k < 100)
 				{
@@ -34,11 +37,11 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
-					_putchar((k / 10) % 10) + '0');
+					_putchar(((k / 10) % 10)+ '0');
 					_putchar((k % 10) + '0');
 				}
 			}
-			_putcahr('\n');
+			_putchar('\n');
 		}
 	}
 }
