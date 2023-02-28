a54503155682bb8109a3f900583dfa9251dae1b8
ezekiel-1 <ezekielmuito671@gmail.com>
2023-02-27 19:32:52 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,24 +2,13 @@
 /**
  * swap_int - swaps the values of two integers
  * @a: integer to swap
- * @: integer to swap
+ * @b: integer to swap
  */
-int swap_int(int *a, int *b)
+void swap_int(in *a, int *b)
 {
-<<<<<<< HEAD
-        int m;
-
-        m = *a;
-        *a = *b;
-        *b = m;
-}
-~
-
-=======
 	int m;
 
 	m = *a;
 	*a = *b;
 	*b = m;
 }
->>>>>>> 2696d37426f1b56f077eeee360cac1a87bb8edd0
