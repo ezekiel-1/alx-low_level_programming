90e95fc6fa440847d99067744c24802a9179d8fa
ezekiel-1 <ezekielmuito671@gmail.com>
2023-03-01 23:04:53 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,14 +18,15 @@ char *cap_string(char *str)
 		str[index - 1] == '\t' ||
 		str[index - 1] == '\n' ||
 		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
 		str[index - 1] == '-' ||
 		str[index - 1] == '!' ||
 		str[index - 1] == '?' ||
 		str[index - 1] == '"' ||
 		str[index - 1] == '(' ||
 		str[index - 1] == ')' ||
-		str[index - 1] == '(' ||
-		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
 		index == 0)
 			str[index] -= 32;
 		index++;
