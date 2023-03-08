3fc260fd98e1d409e3fd8ad1b5db06e550b6a770
ezekiel-1 <ezekielmuito671@gmail.com>
2023-03-08 06:00:31 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,24 @@
+/*
+ * File: 100-is_palindrome.c
+ * Auth: sami
+ */
+#include "main.h"
+
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - Returns the legth of a string.
+ * @s: the string to be measured.
+ * Return: The legth of the string.
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		"100-is_palindrome.c" 67L, 1234C
+	}
+}
