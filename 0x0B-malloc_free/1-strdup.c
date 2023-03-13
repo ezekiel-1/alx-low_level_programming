d9cdccbaf78a892671f64689d02d9b8f2e37229f
ezekiel-1 <ezekielmuito671@gmail.com>
2023-03-13 08:48:05 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,22 +12,19 @@ char *_strdup(char *str)
 	int i = 0, j;
 
 	if (!str)
+		return (NULL);
+	
+	while (*(str + i))
+		i++;
+	i++;
+	s = malloc(sizeof(char) * i);
+	if (s == NULL)
 	{
 		return (NULL);
-
-		while (*(str + i))
-		{
-			i++;
-			i++;
-			s = malloc(sizeof(char) * i);
-
-			if (s == NULL)
-			{
-				return (NULL);
-			}
-			for (j = 0; j <= i; j++)
-			{
-				s[j] == str[j];
-			}
-			return (s);
-		}
+	}
+	for (j = 0; j <= i; j++)
+	{
+		s[j] == str[j];
+	}
+	return (s);
+}
