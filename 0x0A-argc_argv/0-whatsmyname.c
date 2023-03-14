14e4ecedae2d030cc27d49aa1ba28839822785a2
ezekiel-1 <ezekielmuito671@gmail.com>
2023-03-14 00:12:11 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name of the program
+ * @argc: number urguments
+ * @argv: array of urguments
+ * Rturn: Always 0 (Success)
+ */
+int main(int argc _attribute_((unsused)), char *argv[])
+{
+	printf("%\n", *argv);
+
+	return (0);
+}
